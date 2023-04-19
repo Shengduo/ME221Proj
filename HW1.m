c35b33a7c240c8d95c1a9fddd87646a331f3ef9f
Shengduo <sliu5@caltech.edu>
2023-04-19 10:40:41 -0700

    Now works.

--- HW1.m
+++ HW1.m
@@ -1,11 +1,20 @@
 %% Course project for HW1 in ME221
-clear;
+clc, clear;
 close all;
 
 % Define the geometry
 Y = [-1/2, 1/2];
+
+% ------------------- For sub problem 1 ------------------- 
 ns = [0.2, 0.05, 0.01];
 mOverNs = [1., 2.];
+% --------------------------------------------------------- 
+
+% ------------------- For sub problem 2 ------------------- 
+ns = [50];
+mOverNs = [0.005];
+% --------------------------------------------------------- 
+
 NofPts = 1024;
 h = (Y(2) - Y(1)) / (NofPts);
 
@@ -18,26 +27,36 @@ x2 = x2(1 : end - 1);
 alpha = ones(NofPts, NofPts);
 
 % Parameters to start the solve
-U0 = rand(NofPts, NofPts);
+U0 = ones(NofPts, NofPts);
 alpha0 = 1.;
 rtol = 1.e-15;
 max_iters = 10000;
+nOfCases = length(ns) * length(mOverNs);
+aStars = zeros(nOfCases, 2, 2);
+nns = zeros(1, nOfCases);
+mms = zeros(1, nOfCases);
 
 %% Sub 1
 % Loop through all cases
+caseNo = 1;
 for i = 1:1:size(ns, 2)
     for j = 1:1:size(mOverNs, 2) 
         % Get m and n
         n = ns(i);
         m = mOverNs(j) * n;
-        
+        nns(caseNo) = n;
+        mms(caseNo) = m;
+
         % Update alpha
+        alpha = ones(NofPts, NofPts);
         ellipseIdx = ((X1 ./ m) .^ 2 + (X2 ./ n) .^ 2 <= 1.);
         alpha(ellipseIdx) = 2.;
+        spy(alpha - 1);
         divXis = zeros(2, NofPts, NofPts);
         r_errs = zeros(1, 2);
         Us = zeros(2, NofPts, NofPts);
         ress = zeros(1, 2);
+
         % Loop through the xi's
         for d = 1:1:2
             % Compute div(alpha xi)
@@ -48,11 +67,34 @@ for i = 1:1:size(ns, 2)
             r_errs(d) = r_err;
             Us(d, :, :) = U;
             res = computeRes(alpha, U, h, NofPts, divXi);
-            disp(strcat("Res = ", num2str(res)));
+            disp(strcat("Residual = ", num2str(res)));
         end
         
+        % Compute aStar
+        aStar =  zeros(2, 2);
+        for ii = 1:1:2
+            for jj = 1:1:2
+                U = squeeze(Us(jj, :, :));
+                if ii == 1
+                    Ujk = (U([2:NofPts, 1], :) - U([NofPts, 1:NofPts - 1], :)) ./ (2*h);
+                    AikUjk = alpha .* Ujk;
+                elseif ii == 2
+                    Ujk = (U(:, [2:NofPts, 1]) - U(:, [NofPts, 1:NofPts - 1])) ./ (2*h);
+                    AikUjk = alpha .* Ujk;
+                end
+                aStar(ii, jj) = sum(((ii == jj) .* alpha + AikUjk) .* h^2, 'all');
+            end
+        end
+        disp("-------------------------------------------------");
+        disp(strcat("Case No: ", num2str(caseNo)))
+        disp(strcat("m, n = ", num2str([m, n])));
+        disp("aStar:");
+        disp(aStar)
+        aStars(caseNo, :, :) = aStar;
+        caseNo = caseNo + 1;
+
         % Solve the problem using to get Us
-        
+    
     end
 end
 
@@ -66,14 +108,14 @@ function [r_err, U] = solveU(alpha0, alpha, NofPts, h, divXi, U0, rtol, max_iter
     
     % The wave number matrix
     L = h * (NofPts);
-    K = zeros(NofPts, NofPts);
+    % K = zeros(NofPts, NofPts);
     
 %     for i = 1:1:NofPts
 %         for j = 1:1:NofPts
 %             K(i, j) = -((i - 1) ^ 2 + (j - 1) ^ 2) / (L ^ 2) * 4 * pi^2;
 %         end
 %     end
-    
+    % k = (2*pi/L) * [0:1:NofPts - 1];
     k = (2*pi/L)*[0:(NofPts/2-1) (-NofPts/2):(-1)]; % Vector of wavenumbers
     [KX, KY]  = meshgrid(k,k); % Matrix of (x,y) wavenumbers corresponding
                                 % to Fourier mode (m,n)
@@ -81,7 +123,7 @@ function [r_err, U] = solveU(alpha0, alpha, NofPts, h, divXi, U0, rtol, max_iter
     delsq(1,1) = 1;           % Kluge to avoid division by zero of (0,0) waveno.
                                 % of fhat (this waveno. should be zero anyway!)
                             
-    K = 1. ./ delsq;
+    % K = 1. ./ delsq;
     % K(1, 1) = 0.;
     while (iter < max_iters) && (r_err >= rtol)
         % Calculate f
@@ -97,11 +139,11 @@ function [r_err, U] = solveU(alpha0, alpha, NofPts, h, divXi, U0, rtol, max_iter
         iter = iter + 1;
         r_err = norm(U - U_prev, 'fro') / norm(U_prev, 'fro');
         
-        % Check residual now
-        Uxx = (U([2:NofPts, 1], :) - 2 * U + U([NofPts, 1:NofPts - 1], :)) / (h ^ 2);
-        Uyy = (U(:, [2:NofPts, 1]) - 2 * U + U(:, [NofPts, 1:NofPts - 1])) / (h ^ 2);
-        res = (Uxx + Uyy) + f ./ alpha0;
-        disp(strcat("Res in solver: ", num2str(sum(res.^2 * h^2, 'all'))));
+%         % Check residual now
+%         Uxx = (U([2:NofPts, 1], :) - 2 .* U + U([NofPts, 1:NofPts - 1], :)) / (h ^ 2);
+%         Uyy = (U(:, [2:NofPts, 1]) - 2 .* U + U(:, [NofPts, 1:NofPts - 1])) / (h ^ 2);
+%         res = (Uxx + Uyy) + f ./ alpha0;
+%         disp(strcat("Res in solver: ", num2str(sum(res.^2 .* h^2, 'all'))));
     end
     
 end
@@ -159,5 +201,5 @@ function res = computeRes(alpha, U, h, NofPts, divXi)
     divAGradU = squeeze(divAGradU);
     
     % Return residual
-    res = sum((divAGradU + divXi) .^ 2 * h^2, 'all');
+    res = sum((divAGradU + divXi) .^ 2 .* h^2, 'all');
 end
--- shit.m
+++ shit.m
@@ -16,39 +16,48 @@
 %  Script makes a surface plot of u at the Fourier grid points.
   clc,clear;
   close all;
-  N = 2048;      % No. of Fourier modes...should be a power of 2
+  N = 1024;      % No. of Fourier modes...should be a power of 2
   L = 1;       % Domain size (assumed square)
   sig = 0.1;   % Characteristic width of f (make << 1)
-
+  
+  % k = (2*pi/L) .* [0 : 1 : N - 1];
   k = (2*pi/L)*[0:(N/2-1) (-N/2):(-1)]; % Vector of wavenumbers
-  [KX KY]  = meshgrid(k,k); % Matrix of (x,y) wavenumbers corresponding
+  [KX, KY]  = meshgrid(k,k); % Matrix of (x,y) wavenumbers corresponding
                             % to Fourier mode (m,n)
   delsq = -(KX.^2 + KY.^2); % Laplacian matrix acting on the wavenumbers
-  delsq(1,1) = 1;           % Kluge to avoid division by zero of (0,0) waveno.
+  delsq(1,1) = -1;           % Kluge to avoid division by zero of (0,0) waveno.
                             % of fhat (this waveno. should be zero anyway!)
   
   % Construct RHS f(x,y) at the Fourier gridpoints
 
   h = L/N;     % Grid spacing
-  x = (0:(N-1))*h ;
-  y = (0:(N-1))*h;
+  x = (0:(N-1))*h -0.5 * L;
+  y = (0:(N-1))*h - 0.5 * L;
   [X, Y] = meshgrid(x,y);
-  rsq = (X-0.5*L).^2 + (Y-0.5*L).^2;
+  % rsq = (X-0.5*L).^2 + (Y-0.5*L).^2;
+  rsq = X .^ 2 + Y .^ 2;
   sigsq = sig^2;
   f = exp(-rsq/(2*sigsq)).*(rsq - 2*sigsq)/(sigsq^2);
-
+%   %% Add loading f
+%   load('savef.mat');
+%   f = f ./ (-1.);
+%   %%
 %  Spectral inversion of Laplacian
 
   fhat = fft2(f);
+  f_recover = ifft2(fhat);
   u = real(ifft2(fhat./delsq));
   u = u - u(1,1);   % Specify arbitrary constant by forcing corner u = 0.
   
   lapalacian_u = (u([2:N, 1], :) - 2 .* u + u([N, 1:N-1], :)) / h^2 ...
                  + (u(:, [2:N, 1]) - 2 .* u + u(:, [N, 1:N-1])) / h^2;
   residual = sum((lapalacian_u - f) .^ 2 * h^2, 'all');
-  
+  residual_fourier = delsq .* fft2(u) - fhat;
+  residual_fourier(1, 1) = 0.;
+  residual_fourier = sum(residual_fourier .^ 2 * h^2, 'all');
 %  Plot out solution in interior
   disp(strcat("residual = ", num2str(residual)));
+  disp(strcat("residual in fourier = ", num2str(residual_fourier)))
   uex = exp(-rsq/(2*sigsq));
   errmax = norm(u(:)-uex(:),inf)
 %   surf(X,Y,u)
